8bfcaa3beec7cf085e82017d5f3c4f09b6fcad67
Casey Bond <volkswagentruck@gmail.com>
2017-11-10 16:11:32 -0600

    test change color bullet list item text with CSS

---
+++ public/projects/choose-your-own-adventure/cyoa.js
@@ -0,0 +1,11 @@
+var response = prompt("Hi there! Do you want to travel?") || "South America";
+
+var yes = prompt("Visitor") || "Yes";
+
+var no = prompt("Stranger") || "No";
+
+
+
+var output = document.querySelector("#");
+
+output.innerHTML = "<p>  </p>";
