e4655c1673b266184a9bd75ea7addfbffbadc8aa
Casey Bond <volkswagentruck@gmail.com>
2017-11-09 19:50:35 -0600

    add greeting default logic

--- public/media/greeting.js
+++ public/media/greeting.js
@@ -1,5 +1,7 @@
-var name = prompt("Hi there! What's your name?");
+var firstName = prompt("Hi there! What's your first name?") || "Visitor";
+
+var lastName = prompt("What is your last name?") || "McVisitous";
 
 var output = document.querySelector("#greeting");
 
-output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
+  output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + ".</p>";
